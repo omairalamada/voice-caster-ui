7a63d03eda3785529dd7551dae1adca405091aa5
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-05-26 11:04:19 +0800

    adding table for list of offices/colleges

--- src/pages/PopupPage.vue
+++ src/pages/PopupPage.vue
@@ -16,7 +16,7 @@
     </div>
 
     <div class="q-pa-md">
-      <q-card class="my-card">
+      <q-card flat class="my-card">
         <q-tabs v-model="tab" class="text-purple ">
           <q-tab icon="mic" label="Broadcast" name="one" />
           <q-tab icon="people" label="Manage Client" name="two" />
@@ -36,57 +36,25 @@
                   <div class="text-h6">Client/s</div>
                 </q-card-section>
 
-                <q-markup-table>
+                <q-markup-table flat>
                   <thead>
                     <tr>
+                      <th class="text-left">ID</th>
                       <th class="text-left">Colleges/Offices</th>
-                      <th class="text-right">Calories</th>
-                      <th class="text-right">Fat (g)</th>
-                      <th class="text-right">Carbs (g)</th>
-                      <th class="text-right">Protein (g)</th>
-                      <th class="text-right">Sodium (mg)</th>
+                      <th class="text-right">Status</th>
+                      <th class="text-right">Action</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td class="text-left">Frozen Yogurt</td>
-                      <td class="text-right">159</td>
-                      <td class="text-right">6</td>
-                      <td class="text-right">24</td>
-                      <td class="text-right">4</td>
-                      <td class="text-right">87</td>
+                      <td class="text-left">001</td>
+                      <td class="text-left">College Of Information Technology</td>
+                      <td class="text-left">Active</td>
                     </tr>
                     <tr>
-                      <td class="text-left">Ice cream sandwich</td>
-                      <td class="text-right">237</td>
-                      <td class="text-right">9</td>
-                      <td class="text-right">37</td>
-                      <td class="text-right">4.3</td>
-                      <td class="text-right">129</td>
-                    </tr>
-                    <tr>
-                      <td class="text-left">Eclair</td>
-                      <td class="text-right">262</td>
-                      <td class="text-right">16</td>
-                      <td class="text-right">23</td>
-                      <td class="text-right">6</td>
-                      <td class="text-right">337</td>
-                    </tr>
-                    <tr>
-                      <td class="text-left">Cupcake</td>
-                      <td class="text-right">305</td>
-                      <td class="text-right">3.7</td>
-                      <td class="text-right">67</td>
-                      <td class="text-right">4.3</td>
-                      <td class="text-right">413</td>
-                    </tr>
-                    <tr>
-                      <td class="text-left">Gingerbread</td>
-                      <td class="text-right">356</td>
-                      <td class="text-right">16</td>
-                      <td class="text-right">49</td>
-                      <td class="text-right">3.9</td>
-                      <td class="text-right">327</td>
+                      <td class="text-left">002</td>
+                      <td class="text-left">Office Of The President</td>
+                      <td class="text-left">Active</td>
                     </tr>
                   </tbody>
                 </q-markup-table>
