307f2f6992a9b950f438aaccb8b59716b67b2641
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-05-27 13:31:40 +0800

    change color and uncomment the routes for mainlayout

--- src/pages/PopupPage.vue
+++ src/pages/PopupPage.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page padding>
     <div class="q-pa-md">
-      <q-toolbar class="bg-brown text-white" style="width: 600px">
+      <q-toolbar class="bg-indigo-8 text-white" style="width: 600px">
       <q-btn flat round dense icon="mic" />
-      <q-toolbar-title>Voice Caster</q-toolbar-title>
+      <q-toolbar-title>Voice Caster home</q-toolbar-title>
       <q-space />
       <q-btn flat round dense icon="speaker" />
     </q-toolbar>
